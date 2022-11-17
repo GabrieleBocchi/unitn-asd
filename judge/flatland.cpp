5025934913891568205e341a914f1d7c321ddfcc
GabrieleBocchi <gabrielebocchi02@gmail.com>
2022-11-17 13:03:37 +0100

    flatland formatted

--- judge/flatland.cpp
+++ judge/flatland.cpp
@@ -3,22 +3,22 @@
 #include <fstream>
 using namespace std;
 
-bool checkLeft(bool* arr, int start, int end)
+bool checkLeft(bool *arr, int start, int end)
 {
-    if(arr[end] == 1)
+    if (arr[end] == 1)
         return true;
-    for(int i = end - 2; i > start; i = i - 2)
-        if(arr[i] == 1 && arr[i + 1] == 0)
+    for (int i = end - 2; i > start; i = i - 2)
+        if (arr[i] == 1 && arr[i + 1] == 0)
             return true;
     return false;
 }
 
-bool checkRight(bool* arr, int start, int end)
+bool checkRight(bool *arr, int start, int end)
 {
-    if(arr[start] == 0)
+    if (arr[start] == 0)
         return true;
-    for(int i = start + 2; i < end; i = i + 2)
-        if(arr[i] == 0 && arr[i - 1] == 1)
+    for (int i = start + 2; i < end; i = i + 2)
+        if (arr[i] == 0 && arr[i - 1] == 1)
             return true;
     return false;
 }
@@ -33,28 +33,28 @@ int main()
     bool arr[N];
 
     vector<int> result;
-    
+
     char c;
-    for(int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
         in >> c;
-        if(c == 's')
+        if (c == 's')
             arr[i] = true;
         else
             arr[i] = false;
     }
     in.close();
 
-    if(N >= 3)
+    if (N >= 3)
     {
-        if(checkRight(arr, 1, N - 1))
+        if (checkRight(arr, 1, N - 1))
             result.push_back(0);
 
-        for(int i = 2; i < N - 2; i = i + 2)
-            if(checkLeft(arr, 0, i - 1) && checkRight(arr, i + 1, N - 1))
+        for (int i = 2; i < N - 2; i = i + 2)
+            if (checkLeft(arr, 0, i - 1) && checkRight(arr, i + 1, N - 1))
                 result.push_back(i);
 
-        if(checkLeft(arr, 0, N - 2))
+        if (checkLeft(arr, 0, N - 2))
             result.push_back(N - 1);
     }
     else
