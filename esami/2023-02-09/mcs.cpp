0e38597e9c31cc92d28e1d84ad6c72f4cf282678
GabrieleBocchi <gabrielebocchi02@gmail.com>
2023-06-13 16:46:42 +0200

    fix mcs exercise

--- esami/2023-02-09/mcs.cpp
+++ esami/2023-02-09/mcs.cpp
@@ -9,7 +9,7 @@ int mcs(int const *T, int const *U, int n, int m) {
         DP[0][j] = 0;
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= m; j++)
-            if (T[i - 1] == U[j - 1])
+            if (T[i - 1] == U[j - 1] && T[i - 1] > 0)
                 DP[i][j] = DP[i - 1][j - 1] + T[i - 1];
             else
                 DP[i][j] = max(DP[i - 1][j], DP[i][j - 1]);
